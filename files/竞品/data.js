1ad9b82763ff6cae500fe7b39420a2a6c627a251
guozichao <scott19930819@gmail.com>
2025-12-08 21:03:38 +0800

    kongbai

--- files/竞品/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,k)),m,[],n,_(h,o),p,[q],r,_(s,t,u,v,g,w,x,_(),y,[],z,_(A,B,C,D,E,_(F,G,H,I),J,null,K,_(L,D,M,D),N,O,P,null,Q,R,S,T,U,V,W,R,X,Y,Z,_(F,G,H,ba),bb,R,bc,Y,bd,_(be,bf,bg,bh,bi,bh,bj,bh,bk,k,H,_(bl,bm,bn,bm,bo,bm,bp,bq)),i,_(j,k,l,k)),br,_(),bs,_(),bt,_(bu,[])),bv,_(),bw,_());}; 
-var b="url",c="竞品.html",d="generationDate",e=new Date(1765198918099),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=0,l="height",m="adaptiveViews",n="sketchKeys",o="s0",p="variables",q="OnLoadVariable",r="page",s="packageId",t="53cc25ea835d46359c97164fa17b7989",u="type",v="Axure:Page",w="竞品",x="notes",y="annotations",z="style",A="baseStyle",B="627587b6038d43cca051c114ac41ad32",C="pageAlignment",D="near",E="fill",F="fillType",G="solid",H="color",I=0xFFFFFFFF,J="image",K="imageAlignment",L="horizontal",M="vertical",N="imageRepeat",O="auto",P="favicon",Q="sketchFactor",R="0",S="colorStyle",T="appliedColor",U="fontName",V="Applied Font",W="borderWidth",X="borderVisibility",Y="all",Z="borderFill",ba=0xFF797979,bb="cornerRadius",bc="cornerVisibility",bd="outerShadow",be="on",bf=false,bg="offsetX",bh=5,bi="offsetY",bj="blurRadius",bk="spread",bl="r",bm=0,bn="g",bo="b",bp="a",bq=0.349019607843137,br="adaptiveStyles",bs="interactionMap",bt="diagram",bu="objects",bv="masters",bw="objectPaths";
-return _creator();
-})());
